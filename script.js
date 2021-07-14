20f04d5c086a3cb8a729500d22af5855d86da2cf
Nirajak2002 <65244491+Nirajak2002@users.noreply.github.com>
2021-07-14 22:25:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,248 @@
-function  addIP(id,inputType)
+function addNew(ip,classField,field,btn)//,myLabel
 {
-var ip = document.createElement(inputType);               
-document.getElementById(id).appendChild(ip); 
-return false; 
+    let newF=document.createElement(ip);
+    newF.classList.add(classField);
+    let weOb=document.getElementById(field);
+    let weAdd=document.getElementById(btn);
+    weOb.insertBefore(newF,weAdd);
+    return false;
 }
-
 function addExp()
 {
-    var ip1 = document.createElement("input");               
-    document.getElementById('ExpField').appendChild(ip1);  
-    var ip2 = document.createElement("input");               
-    document.getElementById('ExpField').appendChild(ip2);  
-    var startDate=document.createElement("input");   
-    startDate.setAttribute("type","month");
-    document.getElementById('ExpField').appendChild(startDate); 
-    var endDate=document.createElement("input");   
-    endDate.setAttribute("type","month");
-    document.getElementById('ExpField').appendChild(endDate); 
-}
+    let new1=document.createElement("input");
+    let new2=document.createElement("input");
+    let new3=document.createElement("input");
+    let new4=document.createElement("input");
+    let new5=document.createElement("textarea");
+    let lab1=document.createElement("label");
+    let lab2=document.createElement("label");
+    let lab3=document.createElement("label");
+    let lab4=document.createElement("label");
+    let lab5=document.createElement("label");
+    let weOb=document.getElementById("ExpField");
+    let weAdd=document.getElementById("ExpField-add");
+    new3.setAttribute("type","month");
+    new4.setAttribute("type","month");
 
+    new1.classList.add("OrganizationField");
+    lab1.setAttribute("for","new1");
+    lab1.innerHTML="Organization";
+    weOb.insertBefore(lab1,weAdd);
+   weOb.insertBefore(new1,weAdd);
+
+   new2.classList.add("PostField");
+    lab2.setAttribute("for","new2");
+    lab2.innerHTML="Post";
+    weOb.insertBefore(lab2,weAdd);
+  weOb.insertBefore(new2,weAdd);
+
+  new3.classList.add("StartDateField");
+  lab3.setAttribute("for","new3");
+  lab3.innerHTML="Start Date";
+  weOb.insertBefore(lab3,weAdd);
+ weOb.insertBefore(new3,weAdd);
+
+ new4.classList.add("EndDateField");
+ lab4.setAttribute("for","new4");
+ lab4.innerHTML="End date";
+ weOb.insertBefore(lab4,weAdd);
+ weOb.insertBefore(new4,weAdd);
+
+ new5.classList.add("ExpDescField");
+ lab5.setAttribute("for","new5");
+ lab5.innerHTML="Description";
+ weOb.insertBefore(lab5,weAdd);
+ weOb.insertBefore(new5,weAdd);
+ return false;
+}
+ 
 function addEdu()
 {
-    var ip1 = document.createElement("input");               
-    document.getElementById('EduField').appendChild(ip1);  
-    var startDate=document.createElement("input");   
-    startDate.setAttribute("type","month");
-    document.getElementById('EduField').appendChild(startDate); 
-    var endDate=document.createElement("input");   
-    endDate.setAttribute("type","month");
-    document.getElementById('EduField').appendChild(endDate); 
-    var score= document.createElement("input");    
-    score.setAttribute("type","number");           
-    document.getElementById('EduField').appendChild(score);  
+    let new1=document.createElement("input");
+    let new2=document.createElement("input");
+    let new3=document.createElement("input");
+    let new4=document.createElement("input");
+    new2.setAttribute("type","month");
+    new3.setAttribute("type","month");
+    new4.setAttribute("type","number");
+    let lab1=document.createElement("label");
+    let lab2=document.createElement("label");
+    let lab3=document.createElement("label");
+    let lab4=document.createElement("label");
+    let weOb=document.getElementById("EduField");
+    let weAdd=document.getElementById("EduField-add");
+
+    new1.classList.add("InstuField");
+    lab1.setAttribute("for","new1");
+    lab1.innerHTML="Institution";
+    weOb.insertBefore(lab1,weAdd);
+   weOb.insertBefore(new1,weAdd);
+
+   new2.classList.add("InstuFromField");
+    lab2.setAttribute("for","new2");
+    lab2.innerHTML="From";
+    weOb.insertBefore(lab2,weAdd);
+  weOb.insertBefore(new2,weAdd);
+
+  new3.classList.add("InstuToField");
+  lab3.setAttribute("for","new3");
+  lab3.innerHTML="To";
+  weOb.insertBefore(lab3,weAdd);
+ weOb.insertBefore(new3,weAdd);
+
+ new4.classList.add("InstuScoreField");
+  lab4.setAttribute("for","new4");
+  lab4.innerHTML="Score / CGPA";
+  weOb.insertBefore(lab4,weAdd);
+ weOb.insertBefore(new4,weAdd);
+ return false;
 }
 
 function addProj()
 {
-    var ip1 = document.createElement('input');               
-    document.getElementById('ProjField').appendChild(ip1);  
-    var ip1 = document.createElement('input');               
-    document.getElementById('ProjField').appendChild(ip1);  
-    var ta = document.createElement('textarea');               
-    document.getElementById('ProjField').appendChild(ta);  
+    let new1=document.createElement("input");
+    let new2=document.createElement("input");
+    let new3=document.createElement("textarea");
+    let lab1=document.createElement("label");
+    let lab2=document.createElement("label");
+    let lab3=document.createElement("label");
+    let weOb=document.getElementById("ProjField");
+    let weAdd=document.getElementById("ProjectField-add");
+    new1.classList.add("ProjectNameField");
+    lab1.setAttribute("for","new1");
+    lab1.innerHTML="Project Name";
+    weOb.insertBefore(lab1,weAdd);
+   weOb.insertBefore(new1,weAdd);
+
+   new2.classList.add("ProjectTechField");
+    lab2.setAttribute("for","new2");
+    lab2.innerHTML="Technologies used";
+    weOb.insertBefore(lab2,weAdd);
+  weOb.insertBefore(new2,weAdd);
+
+  new3.classList.add("ProjectDescField");
+  lab3.setAttribute("for","new3");
+  lab3.innerHTML="Description";
+  weOb.insertBefore(lab3,weAdd);
+ weOb.insertBefore(new3,weAdd);
+ return false;
 }
 
 function addDomain()
-{
-    var ip1 = document.createElement('input');               
-    document.getElementById('TechField').appendChild(ip1); 
-    var lab = document.createElement('label');  
-    // lab.setAttribute("for","Skills");
-    lab.innerHTML='Skills';
-    document.getElementById('TechField').appendChild(lab); 
-    var ip2 = document.createElement('input');               
-    document.getElementById('TechField').appendChild(ip2); 
-    
+{   
+    let new1=document.createElement("input");
+    let new2=document.createElement("textarea");
+    let weOb=document.getElementById("TechField");
+    let weAdd=document.getElementById("DomainField-add");
+    let lab1=document.createElement("label");
+    let lab2=document.createElement("label");
+    new1.classList.add("DomainField");
+    lab1.setAttribute("for","new1");
+    lab1.innerHTML="Domain";
+    weOb.insertBefore(lab1,weAdd);
+   weOb.insertBefore(new1,weAdd);
+
+   new2.classList.add("SkillsField");
+    lab2.setAttribute("for","new2");
+    lab2.innerHTML="Skills";
+    weOb.insertBefore(lab2,weAdd);
+  weOb.insertBefore(new2,weAdd);
+  return false;
 }
 
 function change()
 
 {
-    console.log("shownig");
+    console.log("start");
     document.getElementById("shrutiHTML").style.display="block";
     document.getElementById("shrutiHTML").scrollIntoView();
-    document.getElementById("font_1").innerHTML=document.getElementById("NameField").value;
-    document.getElementById("font_2").innerHTML=document.getElementById("AboutField").value;
-    let rmt=document.getElementById("ReachAtField");
-    let str="";
-    for(let e of rmt){
-        str=str+ `<li> ${e.value}</li>`;
+   document.getElementById("font_1").innerHTML=document.getElementById("NameField").value;
+   document.getElementById("font_2").innerHTML=document.getElementById("AboutField").value;
+   let wes=document.getElementsByClassName("ReachAtField");
+   let str="";
+    for (let e of wes)
+    {
+            str=str+`<li> ${e.value}</li>`;
     }
-    document.getElementById("reachT").innerHTML=str;
-    return false;
+   document.getElementById("reachT").innerHTML=str;
+   str="";
+   wes=document.getElementsByClassName("AchieveField");
+    for(let e of wes)
+    {
+        str=str+`<li>${e.value}</li>`;
+    }
+    document.getElementById("achieveT").innerHTML=str;
+    wes=document.getElementsByClassName("InterestsField");
+    str3="";
+    for(let e of wes)
+    {
+        str=str+`<li>${e.value}</li>`;
+    }
+    document.getElementById("interestsT").innerHTML=str;
+     wes=document.getElementsByClassName("ProfilesField");
+     str="";
+    for(let e of wes)
+    {
+        str=str+`<li>${e.value}</li>`;
+    }
+    document.getElementById("progprofT").innerHTML=str;
+    wes=document.getElementsByClassName("CWField");
+     str="";
+    for(let e of wes)
+    {
+        str=str+`<li>${e.value}</li>`;
+    }
+    document.getElementById("cwT").innerHTML=str;
+    str="";
+    let wes1=document.getElementsByClassName("OrganizationField");
+    let wes2=document.getElementsByClassName("PostField");
+    let wes3=document.getElementsByClassName("StartDateField");
+    let wes4=document.getElementsByClassName("EndDateField");
+    let wes5=document.getElementsByClassName("ExpDescField");
+    let flg=0;
+    for(i=0;;i++){
+        try{
+            str=str+`<li> ${wes2[i].value}`+" at "+` ${wes1[i].value}`+"<br>From : "+` ${wes3[i].value}`+"  To : "+ ` ${wes4[i].value}`+"<br>"+` ${wes5[i].value}`;
+            flg=1;}
+            catch(ex){flg=0;}
+        if(flg==0)break;
+   }
+   document.getElementById("expT").innerHTML=str;
+   str="";
+   wes1=document.getElementsByClassName("ProjectNameField");
+   wes2=document.getElementsByClassName("ProjectTechField");
+   wes3=document.getElementsByClassName("ProjectDescField");
+   for(i=0;;i++){
+       try{
+           str=str+`<li> ${wes1[i].value}`+"<br>Technologies used : "+` ${wes2[i].value}`+"<br>"+ ` ${wes3[i].value}</li>`;
+           flg=1;}
+           catch(ex){flg=0;}
+       if(flg==0)break;
+  }
+  document.getElementById("projT").innerHTML=str;
+  str="";
+  wes1=document.getElementsByClassName("InstuField");
+  wes2=document.getElementsByClassName("InstuFromField");
+  wes3=document.getElementsByClassName("InstuToField");
+  wes4=document.getElementsByClassName("InstuScoreField");
+  for(i=0;;i++){
+      try{
+          str=str+`<li> ${wes1[i].value}`+"<br>From : "+` ${wes2[i].value}`+"  To : "+ ` ${wes3[i].value}`+"  Score : "+ `${wes4[i].value}</li>`;
+          flg=1;}
+          catch(ex){flg=0;}
+      if(flg==0)break;
+ }
+ document.getElementById("eduT").innerHTML=str;
+ wes1=document.getElementsByClassName("DomainField");
+  wes2=document.getElementsByClassName("SkillsField");
+  str="";
+  for(i=0;;i++){
+    try{
+        str=str+`<li> ${wes1[i].value}`+"<br>        "+` ${wes2[i].value}</li>`;
+        flg=1;}
+        catch(ex){flg=0;}
+    if(flg==0)break;
 }
-
-
-
-
-
-
-
-
-function printCV()
-{
-    document.getElementById("main").style.display="none";
-    window.print();
+document.getElementById("skillsT").innerHTML=str;
+    return false;
 }
